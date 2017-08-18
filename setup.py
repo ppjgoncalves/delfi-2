f904e420560555e07c4a7b6ec60d7a7e16dd3ac8
jan-matthis <mail@jan-matthis.de>
2017-08-18 20:01:30 +0200

    package submodules

--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(
     description='Density estimation likelihood-free inference',
     url='https://github.com/mackelab/delfi',
     author='Mackelab',
-    packages=['delfi'],
+    packages=['delfi', 'delfi.distribution', 'delfi.distribution.mixture',
+              'delfi.generator', 'delfi.inference', 'delfi.kernel',
+              'delfi.neuralnet', 'delfi.neuralnet.layers',
+              'delfi.neuralnet.loss', 'delfi.simulator', 'delfi.summarystats',
+              'delfi.utils'],
     license='BSD',
     install_requires=['dill', 'lasagne', 'numpy', 'scipy', 'theano', 'tqdm'],
     dependency_links=[
