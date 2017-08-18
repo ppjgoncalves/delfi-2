36826a7802389ce0cf75b391c9cebb397da66963
jan-matthis <mail@jan-matthis.de>
2017-08-18 17:29:53 +0200

    author and license

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     version=__version__,
     description='Density estimation likelihood-free inference',
     url='https://github.com/mackelab/delfi',
+    author='Mackelab',
+    license='BSD',
     install_requires=['dill', 'lasagne', 'numpy', 'scipy', 'theano', 'tqdm'],
     dependency_links=[
         'https://github.com/Lasagne/Lasagne/archive/master.zip#egg=lasagne',
