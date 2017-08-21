7681739d4cdad6368dd45ab301751a8cca9eef83
jan-matthis <mail@jan-matthis.de>
2017-08-21 18:07:00 +0100

    pilot run method

--- delfi/inference/BaseInference.py
+++ delfi/inference/BaseInference.py
@@ -15,6 +15,12 @@ class InferenceBase(metaclass=ABCMetaDoc):
     ----------
     generator : generator instance
         Generator instance
+    prior_norm : bool
+        If set to True, will z-transform params based on mean and std of prior
+    pilot_samples : None or int
+        If an integer is provided, a pilot run with the given number of samples
+        is run. The mean and std of the summary statistics of the pilot samples
+        will be subsequently used to z-transform summary statistics.
     seed : int or None
         If provided, random number generator will be seeded
     kwargs : additional keyword arguments
@@ -27,7 +33,8 @@ class InferenceBase(metaclass=ABCMetaDoc):
         training the neural network.
     """
 
-    def __init__(self, generator, seed=None, priorzt=False, **kwargs):
+    def __init__(self, generator, prior_norm=False, pilot_samples=100,
+                 seed=None, **kwargs):
         self.seed = seed
         if seed is not None:
             self.rng = np.random.RandomState(seed=seed)
@@ -47,17 +54,24 @@ class InferenceBase(metaclass=ABCMetaDoc):
         self.network = NeuralNet(**kwargs)
         self.svi = self.network.svi
 
-        # z-transform for stats is set such that it has no effect by default
-        self.params_mean = np.zeros((params.shape[1],))
-        self.params_std = np.ones((params.shape[1],))
-        if priorzt:
+        # parameters for z-transform of params
+        if prior_norm:
             # z-transform for params based on prior
             self.params_mean = self.generator.prior.mean
             self.params_std = self.generator.prior.std
+        else:
+            # parameters are set such that z-transform has no effect
+            self.params_mean = np.zeros((params.shape[1],))
+            self.params_std = np.ones((params.shape[1],))
 
-        # z-transform for stats is set such that it has no effect by default
-        self.stats_mean = np.zeros((stats.shape[1],))
-        self.stats_std = np.ones((stats.shape[1],))
+        # parameters for z-transform for stats
+        if pilot_samples is not None and pilot_samples != 0:
+            # determine via pilot run
+            self.pilot_run(pilot_samples)
+        else:
+            # parameters are set such that z-transform has no effect
+            self.stats_mean = np.zeros((stats.shape[1],))
+            self.stats_std = np.ones((stats.shape[1],))
 
         # observables contains vars that can be monitored during training
         self.compile_observables()
@@ -86,6 +100,12 @@ class InferenceBase(metaclass=ABCMetaDoc):
             return None
         else:
             return self.rng.randint(0, 2**31)
+
+    def pilot_run(self, n_samples):
+        """Pilot run in order to find parameters for z-scoring stats"""
+        params, stats = self.generator.gen(n_samples)
+        self.stats_mean = stats.mean(axis=0)
+        self.stats_std = stats.std(axis=0)
 
     def predict(self, x):
         """Predict posterior given x
