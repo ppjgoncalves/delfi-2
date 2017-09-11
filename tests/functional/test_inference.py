3e3ee1d8312063787b5fdeade5b45d355bfc4579
jan-matthis <mail@jan-matthis.de>
2017-09-11 17:10:31 +0200

    test variances and means

--- tests/functional/test_inference.py
+++ tests/functional/test_inference.py
@@ -7,30 +7,28 @@ import numpy as np
 from delfi.simulator.Gauss import Gauss
 
 
-def test_basic_inference():
-    # TODO: test against ground truth
-
-    n_params = 2
-
-    m = Gauss(dim=n_params)
-    p = dd.Gaussian(m=np.zeros((n_params, )), S=np.eye(n_params))
+def test_basic_inference(n_params=2, seed=42):
+    m = Gauss(dim=n_params, seed=seed)
+    p = dd.Gaussian(m=np.zeros((n_params, )), S=np.eye(n_params), seed=seed)
     s = ds.Identity()
     g = dg.Default(model=m, prior=p, summary=s)
 
     # set up inference
-    res = infer.Basic(g)
+    res = infer.Basic(g, seed=seed)
 
     # run with N samples
-    out = res.run(100)
+    out = res.run(1000)
+
+    # check result
+    posterior = res.predict(np.array([0., 0.]).reshape(1,-1))
+    assert np.allclose(posterior.xs[0].S, np.array([[0.1, 0.0],
+                                                    [0.0, 0.1]]), atol=0.05)
+    assert np.allclose(posterior.xs[0].m, np.array([0.0, 0.0]), atol=0.05)
 
 
-def test_snpe_inference():
-    # TODO: test against ground truth
-
-    n_params = 2
-
-    m = Gauss(dim=n_params)
-    p = dd.Gaussian(m=np.zeros((n_params, )), S=np.eye(n_params))
+def test_snpe_inference(n_params=2, seed=42):
+    m = Gauss(dim=n_params, seed=seed)
+    p = dd.Gaussian(m=np.zeros((n_params, )), S=np.eye(n_params), seed=seed)
     s = ds.Identity()
     g = dg.Default(model=m, prior=p, summary=s)
 
@@ -38,7 +36,13 @@ def test_snpe_inference():
     _, obs = g.gen(1)
 
     # set up inference
-    res = infer.SNPE(g, obs=obs)
+    out = res.run(n_train=1000, n_rounds=1)
 
     # run with N samples
-    out = res.run(n_train=100, n_rounds=2)
+    out = res.run(n_train=1000, n_rounds=2)
+
+    # check result
+    posterior = res.predict(np.array([0., 0.]).reshape(1,-1))
+    assert np.allclose(posterior.xs[0].S, np.array([[0.1, 0.0],
+                                                    [0.0, 0.1]]), atol=0.05)
+    assert np.allclose(posterior.xs[0].m, np.array([0.0, 0.0]), atol=0.05)
